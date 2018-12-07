cd897e3031f0ae504a4f1c6970216ca408025a46
Huyen Pham <huyen.phamhoang96@gmail.com>
2018-12-07 13:55:31 +0700

    query room db

--- app/src/main/java/com/utildev/kotlin/arch/architecturecomponentskotlin/presentation/fragment/room/RoomFragment.kt
+++ app/src/main/java/com/utildev/kotlin/arch/architecturecomponentskotlin/presentation/fragment/room/RoomFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.utildev.kotlin.arch.architecturecomponentskotlin.R
+import com.utildev.kotlin.arch.architecturecomponentskotlin.data.room.model.UserEntity
 import com.utildev.kotlin.arch.architecturecomponentskotlin.databinding.FragmentRoomBinding
 import com.utildev.kotlin.arch.architecturecomponentskotlin.presentation.adapter.UserRoomAdapter
 import com.utildev.kotlin.arch.architecturecomponentskotlin.presentation.fragment.BaseFragment
@@ -18,13 +19,15 @@ import kotlinx.android.synthetic.main.view_list.view.*
 class RoomFragment : BaseFragment() {
     private lateinit var viewModel: RoomViewModel
     private val roomAdapter: UserRoomAdapter by lazy { UserRoomAdapter() }
+    private lateinit var mView: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentRoomBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_room, container, false)
         viewModel = ViewModelProviders.of(this).get(RoomViewModel::class.java)
         binding.viewModel = viewModel
+        mView = binding.root
         init()
-        return binding.root
+        return mView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,5 +46,14 @@ class RoomFragment : BaseFragment() {
                 roomAdapter.setItems(it)
             }
         })
+
+        viewModel.getUserCount()
+        viewModel.userCount!!.observe(this, Observer {
+            mView.fragRoom_tvCount.text = it.toString()
+        })
+
+        mView.fragRoom_fab.setOnClickListener {
+            viewModel.insertUser(UserEntity(0, "Kotlin", "Android Development"))
+        }
     }
 }
