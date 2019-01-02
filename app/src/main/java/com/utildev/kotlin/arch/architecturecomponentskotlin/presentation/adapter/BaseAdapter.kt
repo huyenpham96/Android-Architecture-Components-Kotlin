f2dfbd0552053856b32c3d6060ab7a516fbff61c
Huyen Pham <huyen.phamhoang96@gmail.com>
2019-01-02 10:00:56 +0700

    Update ItemSize Adapter

--- app/src/main/java/com/utildev/kotlin/arch/architecturecomponentskotlin/presentation/adapter/BaseAdapter.kt
+++ app/src/main/java/com/utildev/kotlin/arch/architecturecomponentskotlin/presentation/adapter/BaseAdapter.kt
@@ -18,7 +18,8 @@ open class BaseAdapter(
     private var isLoading = true
 
     companion object {
-        const val VIEW_TYPE_LOADING = 111
+        const val VIEW_TYPE_LOADING = 999
+        const val VIEW_TYPE_NOTHING = 1000
     }
 
     init {
@@ -45,37 +46,43 @@ open class BaseAdapter(
         }
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        return if (viewType == VIEW_TYPE_LOADING) {
-            BaseAdapter.ViewHolder(
-                DataBindingUtil.inflate(
-                    LayoutInflater.from(viewGroup.context), R.layout.view_loadmore, viewGroup, false
-                )
-            )
-        } else {
-            BaseAdapter.ViewHolder(
-                DataBindingUtil.inflate(
-                    LayoutInflater.from(viewGroup.context), layoutId, viewGroup, false
-                )
-            )
-        }
-    }
-
     override fun getItemCount(): Int {
         return if (layoutManager == null) {
             items.size
         } else {
-            items.size + 1
+            if (items.size == 0) items.size else items.size + 1
         }
     }
 
     override fun getItemViewType(position: Int): Int {
         return if (layoutManager != null) {
-            if (position == layoutManager.itemCount - 1) {
-                VIEW_TYPE_LOADING
-            } else position
+            when (//                layoutManager.itemCount <= 1 -> VIEW_TYPE_NOTHING
+                position) {
+                layoutManager.itemCount - 1 -> VIEW_TYPE_LOADING
+                else -> position
+            }
         } else {
             position
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+//            VIEW_TYPE_NOTHING -> BaseAdapter.ViewHolder(
+//                DataBindingUtil.inflate(
+//                    LayoutInflater.from(viewGroup.context), R.layout.view_blank, viewGroup, false
+//                )
+//            )
+            VIEW_TYPE_LOADING -> BaseAdapter.ViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(viewGroup.context), R.layout.view_loadmore, viewGroup, false
+                )
+            )
+            else -> BaseAdapter.ViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(viewGroup.context), layoutId, viewGroup, false
+                )
+            )
         }
     }
 
